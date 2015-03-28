31458e63b1c8126d725ba3682c49bd67845ee213
LimeTheNigga <limethecoder@gmail.com>
2015-03-28 22:03:59 +0200

    Lab "Tree" from me

---
+++ Taras Sakharchuk/Tree.h
@@ -0,0 +1,37 @@
+#ifndef BINARY_TREE
+#define BINARY_TREE
+
+#include <stdio.h>
+
+struct Node {
+	Node(int);
+	Node();
+	~Node();
+
+	int data;
+	Node *left, *right, *parent;
+};
+
+typedef Node* PNode;
+
+class Tree {
+public:
+	Tree();
+	~Tree();
+
+	PNode Insert(int);
+	PNode Search(int);
+	PNode Maximum(PNode);
+	PNode Minimum(PNode);
+	PNode Successor(PNode);
+	PNode Predecessor(PNode);
+
+	void MoveNode(PNode, PNode);
+	void DeleteNode(PNode);
+	void Destroy(PNode);
+
+private:
+	PNode root;
+};
+
+#endif
